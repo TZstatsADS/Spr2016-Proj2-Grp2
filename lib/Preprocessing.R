785c16bad9da078d710a66df23f37e6cda9eb723
Yanyu Zheng <yz2690@columbia.edu>
2016-02-20 17:15:28 -0500

    change in data

--- lib/Preprocessing.R
+++ lib/Preprocessing.R
@@ -11,8 +11,8 @@ setwd("~/Data Science/project2-project2-group2/data")
 colsToKeep = c("Created Date","Incident Zip","X Coordinate (State Plane)","Y Coordinate (State Plane)")
 miceData = fread("Rat_Sightings.csv",select = colsToKeep)
 colnames(miceData) = c("Date","Zip","X","Y")
-miceData %>% 
+miceData = miceData %>% 
   na.omit() %>%
-  transmute(Date = as.Date(Date,"%m/%d/%Y"))
+  mutate(Date = as.Date(Date,"%m/%d/%Y"))
 save(miceData, file = "miceData.RData")  
   

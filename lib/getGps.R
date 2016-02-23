8d2f734318ce0f1a506f8542eb5887e02db4a5b2
Yanyu Zheng <yz2690@columbia.edu>
2016-02-23 18:04:52 -0500

    upload Part1.R

--- lib/getGps.R
+++ lib/getGps.R
@@ -1,13 +1,11 @@
-getGps = function(addr,g){
+getGps = function(addr){
   url = paste('http://maps.google.com/maps/api/geocode/xml?address=', addr,'&sensor=false',sep='')  # construct the URL
   doc = xmlTreeParse(url) 
   root = xmlRoot(doc) 
   lat = xmlValue(root[['result']][['geometry']][['location']][['lat']]) 
   log = xmlValue(root[['result']][['geometry']][['location']][['lng']]) 
-  print("yeah")
   return(c(lat,log))
 }
 
-url = paste('http://maps.google.com/maps/api/geocode/xml?address=', resData$addr[1:15],'&sensor=false',sep='')  # construct the URL
 
 

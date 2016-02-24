2b446e4c3700125ef286a3b4b3a46a2fa2e0cefa
Yanyu Zheng <yz2690@columbia.edu>
2016-02-23 22:09:02 -0500

    add Month to miceData

--- lib/Part1.R
+++ lib/Part1.R
@@ -1,5 +1,5 @@
 #
-n=1
+n=2
 fileName = paste("gps",as.character(n),".RData",sep="")
 
 # Set working directory
@@ -10,7 +10,7 @@ x = c("leaflet","dplyr","data.table","XML")
 lapply(x,library,character.only = TRUE)
 source("../lib/getGps.R")
 load("resData.Rdata")
-Data = resData[((n-1)*2500+1):n*2500,]
+Data = resData[((n-1)*2500+1):(n*2500),]
 gps = lapply(Data$addr,getGps)
 gps = do.call(rbind,gps)
 save(gps,file = fileName)
